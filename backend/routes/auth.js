3bd51175b761e4490518cf27ade452a0b962e8c1
Md. Jewel Rana <m.jewelrana80@gmail.com>
2022-04-27 19:38:57 +0600

    first commit

---
+++ backend/routes/auth.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router()
+const {
+  registerUser,
+  loginUser
+} = require("../controllers/authController");
+
+
+//below post route url
+router.route('/register').post(registerUser);
+router.route('/login').post(loginUser);
+
+
+
+
+module.exports = router;
